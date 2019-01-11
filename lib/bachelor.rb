44a3da4268bfca21bcba09cba0d7eca9df7a3d5a
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 17:58:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,37 +1,3 @@
-data = {
-  "season 30": [
-    {
-      "name":      "Beth Smalls",
-      "age":       "26",
-      "hometown":  "Great Falls, Virginia",
-      "occupation":"Nanny/Freelance Journalist",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Becca Tilley",
-      "age":        "27",
-      "hometown":   "Shreveport, Louisiana",
-      "occupation": "Chiropractic Assistant",
-      "status":     "Eliminated Week 8"
-    }
-  ],
-  "season 29": [
-    {
-      "name":      "Ashley Yeats",
-      "age":       "24",
-      "hometown":  "Denver, Colorado",
-      "occupation":"Dental Assitant",
-      "status":    "Winner"
-    },
-    {
-      "name":       "Sam Grover",
-      "age":        "29",
-      "hometown":   "New York, New York",
-      "occupation": "Entertainer",
-      "status":     "Eliminated Week 6"
-    }
-  ]
-}
 def get_first_name_of_season_winner(data,season)
  data[season.to_sym].each do |into_array|
    if into_array[:status] =="Winner"
