9c98513758f84f8cd8f60252410603ed83b8ed2b
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 17:59:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,39 +1,4 @@
 def get_first_name_of_season_winner(data,season)
- data[season.to_sym].each do |into_array|
-   if into_array[:status] =="Winner"
-     return into_array[:name].split(" ")[0]
-   end
+ data
  end
-end
-
-def get_contestant_name(data, occupation)
-data.each do |season, season_array|
-  season_array.each do |into_array|
-    if into_array[:occupation] == occupation
-      return into_array[:name]
-      end
-    end
-  end
-end
-
-def count_contestants_by_hometown(data, hometown)
-  counter = 0
- data.each do |season, season_array|
-    season_array.each do |into_array|
-      if into_array[:hometown] == hometown
-        counter+=1
-      end
-  end
- end
- return counter
-end
-
-def get_occupation(data, hometown)
-  data.each do |season, season_array|
-    season_array.each do |into_array|
-      if into_array[:hometown] == hometown
-        return into_array[:name]
-      end
-    end
-  end
-end
+ 
