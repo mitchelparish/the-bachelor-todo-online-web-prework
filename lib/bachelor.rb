1085f096b9582969ef27d6b3e342061e9bd1b6ea
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 18:08:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,16 +31,17 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  person = nil
-  data.each do |hash_season, contestants_array|
-    contestants_array.each do |contestant|
-      if contestant["hometown"] == hometown
-        person = contestant["occupation"]
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
       end
     end
   end
-  person
-end
+
+ end
 
 def get_average_age_for_season(data, season)
   contestant_ages = data[season].collect do |contestant|
