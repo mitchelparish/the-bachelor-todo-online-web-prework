843af3191eaaf0464a1b542f6e89e269e2c2ba2b
Mitchel Parish <mitchelparish@gmail.com>
2019-01-11 18:01:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,4 +1,10 @@
 def get_first_name_of_season_winner(data,season)
- data
+ data(season).each do |people|
+   people.each do |k,v|
+     if v == "Winner"
+       full_name = people["name"]
+       return full_name.split(' ').first
+     end
+     
  end
  
